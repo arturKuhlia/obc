34fc2207a2ba9fbc86e0e58c70c000479aa5771a
Artur <artur@pop-os.localdomain>
2020-12-05 16:34:12 -0500

    fukt opens but not work

--- src/app/pages/tabs-page/tabs-page.module.ts
+++ src/app/pages/tabs-page/tabs-page.module.ts
@@ -1,6 +1,28 @@
-import { BlogComponent } from './../../../../../site/src/app/components/blog/blog.component';
-import { BlogEditorComponent } from './../../../../../site/src/app/components/blog-editor/blog-editor.component';
-import { BlogCardComponent } from './../../../../../site/src/app/components/blog-card/blog-card.component';
+  import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+   import { environment } from '../../../environments/environment';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+ import { HttpClientModule } from '@angular/common/http'; 
+ import { ExcerptPipe } from './../../customPipes/excerpt.pipe';
+import { SlugPipe } from './../../customPipes/slug.pipe'; 
+import { MatExpansionModule } from '@angular/material/expansion';
+
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+
+
+
+
+
+
+import { BlogComponent } from './../blog/blog.component';
+import { BlogEditorComponent } from './../blog-editor/blog-editor.component';
+import { BlogCardComponent } from './../blog-card/blog-card.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
@@ -14,16 +36,30 @@ import { ScheduleModule } from '../schedule/schedule.module';
 import { SessionDetailModule } from '../session-detail/session-detail.module';
 import { DocModule } from '../doc/doc.module';
  
+
 @NgModule({
   imports: [
     AboutModule,
     CommonModule,
     IonicModule,
- 
+    
     ScheduleModule,
     SessionDetailModule,
     DocModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+ 
+    IonicModule.forRoot(),
+ 
+    HttpClientModule, 
+    MatExpansionModule,
+    Ng2SearchPipeModule,
+
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+ 
+    CKEditorModule,
+    FormsModule,
   ],
   declarations: [
     TabsPage,
@@ -31,6 +67,10 @@ import { DocModule } from '../doc/doc.module';
     BlogEditorComponent,
     BlogComponent
 
-  ]
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+     ]
 })
 export class TabsModule { }
